5dd97b177907bb473e5637537194f030790b4961
sdurutr436 <sdurutr436@g.educaand.es>
2026-04-14 12:59:23 +0200

    refactor: actualizar imports tras mover header, footer y sidenav a organisms

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './components/header/header';
-import { FooterComponent } from './components/footer/footer';
+import { HeaderComponent } from './components/organisms/header/header';
+import { FooterComponent } from './components/organisms/footer/footer';
 
 @Component({
   selector: 'app-root',
