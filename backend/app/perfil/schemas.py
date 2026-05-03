f4f82b94a6b39172defd9c85bb12eb211d2d1425
sdurutr436 <sdurutr436@g.educaand.es>
2026-05-03 21:14:01 +0200

    feat: añadir NotifTardioConfigSchema en perfil/schemas.py

--- backend/app/perfil/schemas.py
+++ backend/app/perfil/schemas.py
@@ -27,3 +27,16 @@ class XlsxApartamentosConfigSchema(Schema):
     col_nombre     = fields.Int(required=True, validate=validate.Range(min=0, max=200))
     col_direccion  = fields.Int(load_default=0, validate=validate.Range(min=0, max=200))
     col_ciudad     = fields.Int(load_default=0, validate=validate.Range(min=0, max=200))
+
+
+_COL_LETRA_RE = r"^[A-Za-z]{0,2}$"
+_HORA_CORTE_RE = r"^([01]\d|2[0-3]):[0-5]\d$"
+
+
+class NotifTardioConfigSchema(Schema):
+    hora_corte        = fields.Str(required=True, validate=validate.Regexp(_HORA_CORTE_RE))
+    col_nombre        = fields.Str(load_default="", validate=validate.Regexp(_COL_LETRA_RE))
+    col_checkin       = fields.Str(load_default="", validate=validate.Regexp(_COL_LETRA_RE))
+    col_hora_llegada  = fields.Str(load_default="", validate=validate.Regexp(_COL_LETRA_RE))
+    col_telefono      = fields.Str(load_default="", validate=validate.Regexp(_COL_LETRA_RE))
+    col_apartamento   = fields.Str(load_default="", validate=validate.Regexp(_COL_LETRA_RE))
